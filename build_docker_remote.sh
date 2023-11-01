d643cac311889748b9798b5a5262ae8baead9f0d
David Roazen <droazen@broadinstitute.org>
2023-11-01 15:34:02 -0400

    Increase memory on Google Cloud build machine

--- build_docker_remote.sh
+++ build_docker_remote.sh
@@ -9,7 +9,7 @@ IMAGE_VERSION=$1
 DOCKER_REPO="us.gcr.io/broad-dsde-methods/gatk-workshop-terra-jupyter-images/gatk-workshop-terra-jupyter-image"
 DOCKER_IMAGE_TAG="${DOCKER_REPO}:${IMAGE_VERSION}"
 
-gcloud builds submit --tag ${DOCKER_IMAGE_TAG} --timeout=24h --machine-type n1_highcpu_8
+gcloud builds submit --tag ${DOCKER_IMAGE_TAG} --timeout=24h --machine-type n1_highcpu_32
 
 if [ $? -ne 0 ]; then
     echo "gcloud builds submit failed"
